ca5ba53f15d93913aae50dd632a2d7d92120a41c
not4juu <23sebatarnow@gmail.com>
2021-08-20 10:53:45 +0200

    Introduce base environment preparation

---
+++ SimpleProject/SimpleProject.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <memory>
+#include "Scheduler.hpp"
+#include "HelloWorld.hpp"
+
+namespace project
+{	
+
+struct CommandError: std::logic_error
+{
+    CommandError();
+};
+
+class SimpleProject
+{
+public:
+    SimpleProject(Scheduler& scheduler, const std::string& command);
+    SimpleProject(const SimpleProject& ) = delete;
+    SimpleProject& operator=(const SimpleProject&) = delete;
+    
+private:
+    Scheduler& msg_scheduler;
+};
+
+} // namespace project
